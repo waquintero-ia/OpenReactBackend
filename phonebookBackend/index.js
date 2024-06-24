21f14e1e1e3ac3299a5249c99a55434f92711795
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-23 21:28:09 -0500

    3.9

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
+const cors = require('cors')
 
 let persons = [
     { 
@@ -40,7 +41,7 @@ morgan.token('body', request => {
 app.use(express.json())
 app.use(requestLogger)
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
-
+app.use(cors())
 
 
 const unknownEndpoint = (request, response) => {
