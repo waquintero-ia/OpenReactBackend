6794adfb714a252d4c70e41c73e200c988d29286
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-29 15:39:36 -0500

    3.12

---
+++ phonebookBackend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length<3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url =
+  `mongodb+srv://fullstack:${password}@cluster0.alsp754.mongodb.net/phonebookApp?retryWrites=true&w=majority&appName=Cluster0`
+mongoose.set('strictQuery',false)
+
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', noteSchema)
+
+if (process.argv.length === 3){
+    
+    Person.find({}).then(result => {
+        result.forEach(note => {
+          console.log(note)
+        })
+        mongoose.connection.close()
+      })
+}
+
+if (process.argv.length === 5){
+    
+    const people = new Person({
+        name: name,
+        number: number,
+      })
+      
+      people.save().then(result => {
+        console.log(`added ${name} ${number} to phonebook`)
+        mongoose.connection.close()
+      })
+}
