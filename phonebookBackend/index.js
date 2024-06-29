c3687921235bcd16041884035861945ab547b5ba
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-06-29 10:38:03 -0500

    aplicacion a internet con fly.io

--- phonebookBackend/index.js
+++ phonebookBackend/index.js
@@ -119,6 +119,7 @@ app.post('/api/persons', (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
