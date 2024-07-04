3373f294527d8f4609519f69a420c13c9961e22e
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-07-03 19:57:03 -0500

    start

---
+++ blogListBackend/reunion.js
@@ -0,0 +1,33 @@
+//exodo 3:11
+//Entonces Moisés respondió a Dios: 
+//¿Quién soy yo para que vaya a Faraón, 
+//y saque de Egipto a los hijos de Israel?
+
+/*
+Vino a mí palabra de Jehová, diciendo: 2 Hijo de hombre, 
+profetiza contra los pastores de Israel; profetiza, 
+y di a los pastores: Así ha dicho Jehová el Señor: 
+¡Ay de los pastores de Israel, que se apacientan a sí mismos! 
+¿No apacientan los pastores a los rebaños? 3 
+Coméis la grosura, y os vestís de la lana; 
+la engordada degolláis, mas no apacentáis a las ovejas. 
+4 No fortalecisteis las débiles, ni curasteis la enferma; 
+no vendasteis la perniquebrada, no volvisteis al redil 
+la descarriada, ni buscasteis la perdida, 
+sino que os habéis enseñoreado de ellas con dureza y 
+con violencia. 5 Y andan errantes por falta de pastor, 
+y son presa de todas las fieras del campo, y se han dispersado. 
+6 Anduvieron perdidas mis ovejas por todos los montes, 
+y en todo collado alto; y en toda la faz de la tierra fueron esparcidas 
+mis ovejas, y no hubo quien las buscase, ni quien preguntase por ellas.
+7 Por tanto, pastores, oíd palabra de Jehová: 
+8 Vivo yo, ha dicho Jehová el Señor, que por cuanto mi rebaño fue para ser robado, 
+y mis ovejas fueron para ser presa de todas las fieras del campo, sin pastor; 
+ni mis pastores buscaron mis ovejas, sino que los pastores 
+se apacentaron a sí mismos, y no apacentaron mis ovejas; 
+9 por tanto, oh pastores, oíd palabra de Jehová. 
+10 Así ha dicho Jehová el Señor: He aquí, yo estoy contra los pastores; 
+y demandaré mis ovejas de su mano, y les haré dejar de apacentar las ovejas; 
+ni los pastores se apacentarán más a sí mismos, 
+pues yo libraré mis ovejas de sus bocas, y no les serán más por comida.
+*/
