23856e925182fac2ae48b6fada2005ed9a47b49b
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-07-03 21:51:08 -0500

    4.1-4.2(2)

--- blogListBackend/models/blog.js
+++ blogListBackend/models/blog.js
@@ -3,7 +3,15 @@ const mongoose = require('mongoose')
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
-    url: String,
+    url: {
+      type: String,
+      validate:{
+        validator: function(v){
+          return /(?:https?):\/\/(\w+:?\w*)?(\S+)(:\d+)?(\/|\/([\w#!:.?+=&%!\-\/]))?/.test(v)
+        },
+        message: 'Enter a correct url'
+      }
+    },
     likes: Number
 })
 
