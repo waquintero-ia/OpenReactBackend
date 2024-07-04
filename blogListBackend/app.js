a5db09ede2712c9307bf67dfa95ffb33e4179202
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-07-03 21:43:57 -0500

    4.1-4.2

--- blogListBackend/app.js
+++ blogListBackend/app.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const mongoose = require('mongoose')
 
+mongoose.set('strictQuery', false)
 
+logger.info('connecting to', config.MONGODB_URI)
 
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.morgan(':method :url :status :res[content-length] :response-time ms :body'))
 
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
