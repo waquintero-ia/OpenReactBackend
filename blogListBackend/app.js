44e53dccc750d3fc638b38f01060d56f6c2ac8ce
Wilson Andres Quintero Cardona <81442287+waquintero-ia@users.noreply.github.com>
2024-07-13 15:25:22 -0500

    4.15

--- blogListBackend/app.js
+++ blogListBackend/app.js
@@ -3,6 +3,8 @@ const app = express()
 require('express-async-errors')
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
@@ -33,6 +35,8 @@ if(process.env.NODE_ENV !== 'test' ){
 }
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- blogListBackend/models/blog.js
+++ blogListBackend/models/blog.js
@@ -16,7 +16,11 @@ const blogSchema = new mongoose.Schema({
         message: 'Enter a correct url'
       }
     },
-    likes: Number
+    likes: Number,
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User'
+    }
 })
 
 blogSchema.set('toJSON', {
